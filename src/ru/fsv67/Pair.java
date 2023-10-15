26c0c72a4d517f9e0e05c709e105a789620418ff
Sergey Filippenkov <fsvmxd@yandex.ru>
2023-10-15 23:34:29 +0300

    homework completed

---
+++ src/ru/fsv67/Pair.java
@@ -0,0 +1,30 @@
+package ru.fsv67;
+
+/*
+ * Напишите обабщенный класс Pair, который представляет собой пару значений разного типа.
+ * Класс должен иметь методы getFirst() и getSecond() для получения значений пары, а также
+ * переопределение метода toString(), возвращающее строковое представление пары
+ */
+public class Pair<T, E> {
+    T first;
+    E second;
+
+    public Pair(T first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "first: " + getFirst() + ", Second: " + getSecond();
+    }
+
+}
